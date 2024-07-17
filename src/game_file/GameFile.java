826648ce063ab6b628616d7cb8960c3274e11440
Janson <hohohojanson@gmail.com>
2024-07-17 14:29:06 +0800

    initial ui bug fixing

--- src/game_file/GameFile.java
+++ src/game_file/GameFile.java
@@ -5,8 +5,8 @@ import java.io.Serializable;
 public class GameFile implements Serializable {
     int gameSlot;
     int map;
-    int playerX;
-    int playerY;
+    int playerX= 400;
+    int playerY = 400;
     //Weapon weapon;
 
 }
