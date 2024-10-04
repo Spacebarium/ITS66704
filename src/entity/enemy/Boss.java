3595bc87258a0d2c36d8618f35bc0afebb61e137
LeeJon94 <lewleejon040915@gmail.com>
2024-10-04 16:01:28 +0800

    Merge commit testing
    Signed-off-by: LeeJon94 <lewleejon040915@gmail.com>

--- src/entity/enemy/Boss.java
+++ src/entity/enemy/Boss.java
@@ -9,9 +9,10 @@ public class Boss extends Enemy {
         super(gp, "Boss", health, damage, defaultX, defaultY, 32 * gp.getScale(), 32 * gp.getScale(), 5 * gp.getScale(), 3 * gp.getScale(), 21 * gp.getScale(), 27 * gp.getScale());
         getImage();
     }
-//hello testing 1, 2, 3
+    //hello testing 1, 2, 3
     //hero wars peak
     //hello darkness my old friends
+    //nani???
 
     public void getImage(){
         String folderName = "entity/enemy/boss";
